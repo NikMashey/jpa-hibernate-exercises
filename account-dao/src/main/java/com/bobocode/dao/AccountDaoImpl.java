3363e86828b45478ddf2b8f9ce5c83a09915e478
NikMashey <nik-mashej6@yandex.ru>
2020-03-28 21:59:30 +0300

    [account-dao] implement method for crud operations

--- account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -1,7 +1,10 @@
 package com.bobocode.dao;
 
+import com.bobocode.exception.AccountDaoException;
 import com.bobocode.model.Account;
+import com.google.inject.internal.cglib.core.$DebuggingClassWriter;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
@@ -14,32 +17,61 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.persist(account);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("account is invalid", e);
+        }
     }
 
     @Override
     public Account findById(Long id) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        return entityManager
+                .createQuery("select a from Account a where a.id = :id", Account.class)
+                .setParameter("id", id)
+                .getSingleResult();
     }
 
     @Override
     public Account findByEmail(String email) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        return entityManager.createQuery("select a from Account a where a.email = :email", Account.class)
+                .setParameter("email", email)
+                .getSingleResult();
     }
 
     @Override
     public List<Account> findAll() {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        return entityManager.createQuery("select a from Account a", Account.class)
+                    .getResultList();
     }
 
     @Override
     public void update(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(account);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("account is invalid", e);
+        }
     }
 
     @Override
     public void remove(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+       entityManager.getTransaction().begin();
+        Account managedAccount = entityManager.merge(account);
+        entityManager.remove(managedAccount);
+       entityManager.getTransaction().commit();
     }
 }
 
